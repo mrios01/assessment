308895d78c83a41ca1e46755e32ebbe7da1347e6
plug010 <m.angel.rios@gmail.com>
2021-03-29 01:56:31 -0600

    Tests
    Tested different numbers into phrases

--- js-numerals/src/app/app.component.spec.ts
+++ js-numerals/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('js-numerals app is running!');
+    expect(compiled.querySelector('h3').textContent).toContain('Miguel Angel Ríos Arias');
   });
 });
